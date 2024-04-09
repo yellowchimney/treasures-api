3359155ea5ccb11e1b312a594a471869b93b5022
DCrawley94 <danikacrawley.dev@gmail.com>
2024-04-09 14:34:08 +0100

    update example .env and refactor setup_dbs

--- db/run_seed.py
+++ db/run_seed.py
@@ -1,14 +1,10 @@
 '''This module contains the logic to seed the development databases
 for the `Cat's Rare Treasures` FastAPI app.'''
 from seed import seed_db
-from subprocess import run
 
 
 def main() -> None:
-    '''This function runs the `setup_dbs` SQL script
-    and the `seed_db` function'''
-    # Create test and dev databases
-    run('psql -f db/setup_dbs.sql', shell=True)
+    '''This function runs the `seed_db` function'''
     seed_db()
 
 
